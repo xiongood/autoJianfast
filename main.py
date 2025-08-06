f5e177d446894b8d77b3da9546ac399c09cf8bc0
张一雄 <xiongood@88.com>
2025-08-06 15:45:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from lxml import etree
 import random
 import requests
+import smtplib
+from email.mime.text import MIMEText
 
 # 获取第一页 图片地址
 srcs = []
-#url = r'https://www.yaash.cn/tag/second-dimension/'#分类
-#url = r'https://www.yaash.cn/?cat=&s=%E6%B8%85%E7%BA%AF'#关键字搜索
+
 url = r'https://www.yaash.cn/'#主页
 response = requests.get(url)
-# print(response.status_code)
+
 # 检查请求是否成功
 if response.status_code == 200:
     response.encoding = 'GBK'
@@ -16,23 +17,41 @@ if response.status_code == 200:
     # print(content)
     tree = etree.HTML(content)
     srcs = tree.xpath("//img[contains(@style, 'object-fit: cover;')]/@src")
-# for src in srcs:
-#     print( src)
+
 src = random.choice(srcs)
-# print(src)
+
 # 替换
 src = src.replace('@small', '@middle')
-#
-# 目标URL
-url = r"https://www.jianfast.com/home/bg/show?type=update&picurl="+src
-print(url)
-# 拼装Cookie（字典形式）
+
+# # 拼装Cookie（字典形式）
 headers = {
-    "Cookie": "wel_code=1; u=b31de6781203a97eb0b17b388e6d91ab; Hm_lvt_7cfb32da5a4a5240b600dddec4023a51=1752626936,1752627273,1752627864,1752628707; HMACCOUNT=6B860EBD77E2A2E0; PHPSESSID=0cuvgq2qcv7hqomspotb1aefdc; Hm_lpvt_7cfb32da5a4a5240b600dddec4023a51=1752628765",
+    "cookie": "wel_code=1; u=b31de6781203a97eb0b17b388e6d91ab; act=1; Hm_lvt_7cfb32da5a4a5240b600dddec4023a51=1754461713,1754461791,1754461911,1754463844; HMACCOUNT=D3BBBE66B6CCC6D8; PHPSESSID=lelo28sn929ul2pnjfhppcth03; loadingbg=https%3A//img.netbian.com/file/2024/0325/234506wB8aC.jpg; Hm_lpvt_7cfb32da5a4a5240b600dddec4023a51=1754464974",
 }
 
-# 发送带有Cookie的GET请求
-response = requests.get(url, headers=headers)
+data = {
+    "bg":src
+}
+# 发送post请求
+response = requests.post(r'https://www.jianfast.com/home/bg/save', data,headers=headers)
+print(response.status_code)
+if response.status_code == 200:
+    print("保存成功")
+else:
+    print("保存失败")
+    # 发送邮件
+    """发送纯文字邮件"""
+    # 创建邮件内容
+    msg = MIMEText('简法壁纸发送失败'+src, 'plain', 'utf-8')
+    msg['From'] = 'java0417@163.com'
+    msg['To'] = 'xiongood@88.com'
+    msg['Subject'] = 'github Actions 通知'
 
-
-
+    try:
+        # 163邮箱的465端口需要用SMTP_SSL而非starttls
+        with smtplib.SMTP_SSL('smtp.163.com', 465) as server:
+            # 不需要调用starttls()，SSL连接本身就是加密的
+            server.login('java0417@163.com', 'HLYAyKmG4Fg4jskQ')
+            server.send_message(msg)
+        print("邮件发送成功！")
+    except Exception as e:
+        print(f"发送失败: {str(e)}")
